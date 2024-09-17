744af828b8101f9c97e3e86b8a6a622e2f1ef49c
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2024-09-17 01:09:58 -0700

    Create a pleasant menu that will be used to switch between scenes

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+import { initMenuFunctionality } from "./lib/menu";
+import { resizeElement } from "./lib/resize";
 import { createScene, loopScene } from "./scene/the-aviator";
 
-onload = () => {
-  // Setup 3D engine
-  W.reset(c);
-
+const canvas = document.getElementById("c");
+const theAviatorSetup = () => {
+  W.reset(canvas);
   createScene();
   loopScene();
+};
+
+onload = () => {
+  initMenuFunctionality();
+  resizeElement(canvas);
+  theAviatorSetup();
 };
